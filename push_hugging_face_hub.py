2dbcab58e02b6c9ae338ada55e33f63610befaf9
auto <nishikawa@gpuhost06.cm.cluster>
2021-12-28 15:32:08 +0900

    add/push_hugging_face_hub

---
+++ push_hugging_face_hub.py
@@ -0,0 +1,23 @@
+
+from transformers import AutoModel, AutoTokenizer, XLMRobertaTokenizer
+import argparse
+
+# please update the version of transformers when you use this code
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_name_or_path", type=str)    
+    parser.add_argument("save_name", type=str)     
+    args = parser.parse_args()
+
+    model = AutoModel.from_pretrained(args.model_name_or_path)
+#     if "xlm" in args.model_name_or_path:
+#         tokenizer = XLMRobertaTokenizer.from_pretrained(args.model_name_or_path)
+#     else:
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_fast=True)
+
+    model.push_to_hub(args.save_name, use_temp_dir=True)
+    tokenizer.push_to_hub(args.save_name, use_temp_dir=True)
+
+if __name__ == "__main__":
+    main()
