02b3ae22cbc1b8de66b689a451fc1244a7cf2ec1
auto <nishikawa@gpuhost06.cm.cluster>
2021-12-28 15:31:51 +0900

    add/glove

--- text-clustering/evaluate.py
+++ text-clustering/evaluate.py
@@ -1,11 +1,14 @@
 def warn(*args, **kwargs):
     pass
+
+
 import warnings
+
 warnings.warn = warn
 
 from transformers import AutoModel, AutoTokenizer, XLMRobertaTokenizer
 from coclust.evaluation.external import accuracy
-from sklearn.cluster import KMeans 
+from sklearn.cluster import KMeans
 import torch
 import torch.nn as nn
 import numpy as np
@@ -15,9 +18,12 @@ from prettytable import PrettyTable
 from tqdm import tqdm
 from omegaconf import OmegaConf
 from utils.mlflow_writer import MlflowWriter
+from utils.glove import GloveSentenceEncoder
 
 from sklearn.metrics.cluster import contingency_matrix
 from munkres import Munkres
+
+
 def get_label_mapping_dict(list1, list2):
     mapping_dict = dict()
     m = Munkres()
@@ -32,13 +38,15 @@ def get_label_mapping_dict(list1, list2):
 
 import os
 
-os.chdir('/home/fmg/nishikawa/EASE/text-clustering')
+os.chdir("/home/fmg/nishikawa/EASE/text-clustering")
+
 
 def print_table(task_names, scores):
     tb = PrettyTable()
     tb.field_names = task_names
     tb.add_row(scores)
     print(tb)
+
 
 def batcher(model, tokenizer, sentence, args, device="cuda"):
     # encode_dict = tokenizer.batch_encode_plus(sentence, pad_to_max_length=True, add_special_tokens=True, return_tensors="pt",)
@@ -61,22 +69,31 @@ def batcher(model, tokenizer, sentence, args, device="cuda"):
             pooler_output = outputs.pooler_output
 
     # Apply different poolers
-    if args.pooler == 'cls':
+    if args.pooler == "cls":
         # There is a linear+activation layer after CLS representation
         return pooler_output.cpu()
-    elif args.pooler == 'cls_before_pooler':
+    elif args.pooler == "cls_before_pooler":
         return last_hidden[:, 0].cpu()
     elif args.pooler == "avg":
-        return ((last_hidden * batch['attention_mask'].unsqueeze(-1)).sum(1) / batch['attention_mask'].sum(-1).unsqueeze(-1)).cpu()
+        return (
+            (last_hidden * batch["attention_mask"].unsqueeze(-1)).sum(1)
+            / batch["attention_mask"].sum(-1).unsqueeze(-1)
+        ).cpu()
     elif args.pooler == "avg_first_last":
         first_hidden = hidden_states[0]
         last_hidden = hidden_states[-1]
-        pooled_result = ((first_hidden + last_hidden) / 2.0 * batch['attention_mask'].unsqueeze(-1)).sum(1) / batch['attention_mask'].sum(-1).unsqueeze(-1)
+        pooled_result = (
+            (first_hidden + last_hidden) / 2.0 * batch["attention_mask"].unsqueeze(-1)
+        ).sum(1) / batch["attention_mask"].sum(-1).unsqueeze(-1)
         return pooled_result.cpu()
     elif args.pooler == "avg_top2":
         second_last_hidden = hidden_states[-2]
         last_hidden = hidden_states[-1]
-        pooled_result = ((last_hidden + second_last_hidden) / 2.0 * batch['attention_mask'].unsqueeze(-1)).sum(1) / batch['attention_mask'].sum(-1).unsqueeze(-1)
+        pooled_result = (
+            (last_hidden + second_last_hidden)
+            / 2.0
+            * batch["attention_mask"].unsqueeze(-1)
+        ).sum(1) / batch["attention_mask"].sum(-1).unsqueeze(-1)
         return pooled_result.cpu()
     else:
         raise NotImplementedError
@@ -85,17 +102,20 @@ def batcher(model, tokenizer, sentence, args, device="cuda"):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model_name_or_path", type=str, help="Transformers' model name or path", default="bert-base-uncased"
+        "--model_name_or_path",
+        type=str,
+        help="Transformers' model name or path",
+        default="bert-base-uncased",
     )
     parser.add_argument(
         "--pooler",
         type=str,
         choices=["cls", "cls_before_pooler", "avg", "avg_top2", "avg_first_last"],
         default="cls_before_pooler",
-#         default="avg",
+        #         default="avg",
         help="Which pooler to use",
     )
-    
+
     parser.add_argument(
         "--task_set",
         type=str,
@@ -110,7 +130,7 @@ def main():
         default="evals",
         help="mlflow experiment name",
     )
-    
+
     parser.add_argument(
         "--seed",
         type=int,
@@ -118,11 +138,11 @@ def main():
         help="seed for Kmeans",
     )
 
-    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument("--verbose", action="store_true")
 
     args = parser.parse_args()
     print("model_path", args.model_name_or_path)
-    
+
     # mlflow
     cfg = OmegaConf.create({"eval_args": vars(args)})
     EXPERIMENT_NAME = args.experiment_name
@@ -131,43 +151,42 @@ def main():
     mlflow_writer.log_params_from_omegaconf_dict(cfg)
 
     # Load transformers' model checkpoint
-    model = AutoModel.from_pretrained(args.model_name_or_path)
-    if "xlm" in args.model_name_or_path:
-        tokenizer = XLMRobertaTokenizer.from_pretrained(args.model_name_or_path)
-    else:
-        tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
-    # datasets = ["tweet", "biomedical", "stackoverflow", "searchsnippets", "agnews", "googlenews_ts", "googlenews_t", "googlenews_s"] 
-    # datasets = []
+    if args.model_name_or_path != "glove":
+        model = AutoModel.from_pretrained(args.model_name_or_path)
+        if "xlm" in args.model_name_or_path:
+            tokenizer = XLMRobertaTokenizer.from_pretrained(args.model_name_or_path)
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        model = model.to(device)
     dataset_groups = []
     if args.task_set in ["full", "mono"]:
-        dataset_groups.append(["R8", "R52", "OH", '20N'])
-        dataset_groups.append(['AG', 'SS', 'SO', 'Bio', 'Tweet', 'G-TS', 'G-S', 'G-T'])
-    
+        # dataset_groups.append(["R8", "R52", "OH", "20N"])
+        dataset_groups.append(["AG", "SS", "SO", "Bio", "Tweet", "G-TS", "G-S", "G-T"])
+
     if args.task_set in ["full", "cl"]:
 
-        # 15 langs label-unified 
-        # dataset_groups.append(["WN-unified"])
-        # todo add en
-        dataset_groups.append(['WN-FS-ar', 'WN-FS-ca', 'WN-FS-cs', 'WN-FS-de',  'WN-FS-en', 'WN-FS-eo', 'WN-FS-es', 'WN-FS-fa', 'WN-FS-fr', 'WN-FS-ko', 'WN-FS-ja', 'WN-FS-pl', 'WN-FS-pt', 'WN-FS-ru', 'WN-FS-sv', 'WN-FS-tr'])
-        # dataset_groups.append(['WN-TFS-ar', 'WN-TFS-ca', 'WN-TFS-cs', 'WN-TFS-de', 'WN-TFS-es', 'WN-TFS-eo', 'WN-TFS-fa', 'WN-TFS-fr', 'WN-TFS-ko', 'WN-TFS-ja', 'WN-TFS-pl', 'WN-TFS-pt', 'WN-TFS-ru', 'WN-TFS-sv', 'WN-TFS-tr'])
-        # dataset_groups.append(['WN-S-ar', 'WN-S-ca', 'WN-S-cs', 'WN-S-de', 'WN-S-es', 'WN-S-eo', 'WN-S-fa', 'WN-S-fr', 'WN-S-ko', 'WN-S-ja', 'WN-S-pl', 'WN-S-pt', 'WN-S-ru', 'WN-S-sv', 'WN-S-tr'])
-        # dataset_groups.append(['WN-T-ar', 'WN-T-ca', 'WN-T-cs', 'WN-T-de', 'WN-T-es', 'WN-T-eo', 'WN-T-fa', 'WN-T-fr', 'WN-T-ko', 'WN-T-ja', 'WN-T-pl', 'WN-T-pt', 'WN-T-ru', 'WN-T-sv', 'WN-T-tr'])
-        # dataset_groups.append(['WN-TS-ar', 'WN-TS-ca', 'WN-TS-cs', 'WN-TS-de', 'WN-TS-es', 'WN-TS-eo', 'WN-TS-fa', 'WN-TS-fr', 'WN-TS-ko', 'WN-TS-ja', 'WN-TS-pl', 'WN-TS-pt', 'WN-TS-ru', 'WN-TS-sv', 'WN-TS-tr'])
-        # dataset_groups.append(['WN-S-en', 'WN-S-ja', 'WN-S-es', 'WN-S-tr', 'WN-S-ar'])
-        # dataset_groups.append(['WN-T-en', 'WN-T-de', 'WN-T-fr'])
-        # 13 langs
-        # dataset_groups.append(['WN-T-en', 'WN-T-ar', 'WN-T-ja', 'WN-T-es', 'WN-T-tr', 'WN-T-it', 'WN-T-ko', 'WN-T-pl', 'WN-T-fa', 'WN-T-nl', 'WN-T-ru'])
-        # dataset_groups.append(['WN-S-en', 'WN-S-ar', 'WN-S-ja', 'WN-S-es', 'WN-S-tr', 'WN-S-it', 'WN-S-ko', 'WN-S-pl', 'WN-S-fa', 'WN-S-nl', 'WN-S-ru'])
-        # dataset_groups.append(['WN-TS-en', 'WN-TS-ar', 'WN-TS-ja', 'WN-TS-es', 'WN-TS-tr', 'WN-TS-it', 'WN-TS-ko', 'WN-TS-pl', 'WN-TS-fa', 'WN-TS-nl', 'WN-TS-ru'])
-        
-        # NC
-        # dataset_groups.append(["NC-en", "NC-de", "NC-es"])
-
-        # dataset_groups.append(['WN-T-en', 'WN-T-ar', 'WN-T-ja', 'WN-T-es', 'WN-T-tr', 'WN-T-it', 'WN-T-ko', 'WN-T-pt', 'WN-T-uk', 'WN-T-cs', 'WN-T-pl', 'WN-T-ca', 'WN-T-fi', 'WN-T-fa', 'WN-T-nl', 'WN-T-hu', 'WN-T-eo', 'WN-T-ru'])
-        # dataset_groups.append(['WN-S-en', 'WN-S-ar', 'WN-S-ja', 'WN-S-es', 'WN-S-tr', 'WN-S-it', 'WN-S-ko', 'WN-S-pt', 'WN-S-uk', 'WN-S-cs', 'WN-S-pl', 'WN-S-ca', 'WN-S-fi', 'WN-S-fa', 'WN-S-nl', 'WN-S-hu', 'WN-S-eo', 'WN-S-ru'])
-        # dataset_groups.append(['WN-TS-en', 'WN-TS-ar', 'WN-TS-ja', 'WN-TS-es', 'WN-TS-tr', 'WN-TS-it', 'WN-TS-ko', 'WN-TS-pt', 'WN-TS-uk', 'WN-TS-cs', 'WN-TS-pl', 'WN-TS-ca', 'WN-TS-fi', 'WN-TS-fa', 'WN-TS-nl', 'WN-TS-hu', 'WN-TS-eo', 'WN-TS-ru'])
+        # 15 langs label-unified
+        dataset_groups.append(
+            [
+                "WN-FS-ar",
+                "WN-FS-ca",
+                "WN-FS-cs",
+                "WN-FS-de",
+                "WN-FS-en",
+                "WN-FS-eo",
+                "WN-FS-es",
+                "WN-FS-fa",
+                "WN-FS-fr",
+                "WN-FS-ko",
+                "WN-FS-ja",
+                "WN-FS-pl",
+                "WN-FS-pt",
+                "WN-FS-ru",
+                "WN-FS-sv",
+                "WN-FS-tr",
+            ]
+        )
     batch_size = 256
 
     results = []
@@ -182,26 +201,56 @@ def main():
             if args.verbose:
                 print("encode sentence embeddings...")
             sentence_embeddings = []
-            for i in tqdm(range(0, len(sentences), batch_size)):
-                sentence_embeddings.append(batcher(model, tokenizer, sentences[i: i + batch_size], args, device="cuda"))
-            sentence_embeddings = torch.cat(sentence_embeddings, dim=0)
+
+            if args.model_name_or_path == "glove":
+
+                glove_sentence_encoder = GloveSentenceEncoder(
+                    "/home/fmg/nishikawa/corpus/glove/glove.6B.300d.txt"
+                )
+                sentence_embeddings = torch.tensor(
+                    glove_sentence_encoder.encode_sentences(sentences)
+                )
+
+            else:
+                for i in tqdm(range(0, len(sentences), batch_size)):
+                    sentence_embeddings.append(
+                        batcher(
+                            model,
+                            tokenizer,
+                            sentences[i : i + batch_size],
+                            args,
+                            device="cuda",
+                        )
+                    )
+                sentence_embeddings = torch.cat(sentence_embeddings, dim=0)
 
             # sentence_embeddings = batcher(model, tokenizer, sentences, args, device="cuda")
             if args.verbose:
                 print("clutering...")
-            kmeans_model = KMeans(n_clusters=len(set(labels)), random_state=args.seed).fit(sentence_embeddings)
+            kmeans_model = KMeans(
+                n_clusters=len(set(labels)), random_state=args.seed
+            ).fit(sentence_embeddings)
             pred_labels = kmeans_model.labels_
-            
+
             if dataset_key == "WN-unified":
                 datasets = []
                 mapping_dict = get_label_mapping_dict(pred_labels, labels)
                 pred_labels = [mapping_dict[pred_label] for pred_label in pred_labels]
 
                 for lang, start_idx, end_idx in lang_pos:
-                    acc = np.sum((np.array(labels[start_idx:end_idx]) == pred_labels[start_idx:end_idx])) / len(pred_labels[start_idx:end_idx]) * 100
+                    acc = (
+                        np.sum(
+                            (
+                                np.array(labels[start_idx:end_idx])
+                                == pred_labels[start_idx:end_idx]
+                            )
+                        )
+                        / len(pred_labels[start_idx:end_idx])
+                        * 100
+                    )
                     # acc = accuracy(labels[start_idx:end_idx], pred_labels[start_idx:end_idx]) * 100
                     scores.append("%.2f" % (acc))
-                    mlflow_writer.log_metric(f"{dataset_key}-{lang}", acc) 
+                    mlflow_writer.log_metric(f"{dataset_key}-{lang}", acc)
                     datasets.append(f"{dataset_key}-{lang}")
                 break
 
@@ -214,8 +263,10 @@ def main():
         datasets.append("Avg.")
         scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
         results.append((datasets, scores))
-        mlflow_writer.log_metric(f"{datasets[0]}-Avg.", sum([float(score) for score in scores]) / len(scores))
-        
+        mlflow_writer.log_metric(
+            f"{datasets[0]}-Avg.", sum([float(score) for score in scores]) / len(scores)
+        )
+
     for result in results:
         datasets, scores = result
         print("------ %s ------" % (datasets[0]))
